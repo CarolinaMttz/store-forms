a64f1f7ecd77c944bd8df77153ee2008f4365f47
Dulce Carolina Martínez Carrera <dulce.martinez@ulsaoaxaca.edu.mx>
2023-01-17 16:00:11 -0600

    Clase Usando FormGroup para agrupar multiples campos

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 
 @Component({
@@ -8,7 +8,24 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./basic-form.component.scss']
 })
 export class BasicFormComponent implements OnInit {
+   /* Ejemplo de uso de FormGroup */
+  form = new FormGroup({
+    name:        new FormControl('', [ Validators.required, Validators.maxLength(10) ]),
+    email:       new FormControl('', [ Validators.required, Validators.email ]),
+    phone:       new FormControl(''),
+    color:       new FormControl('#000000'),
+    date:        new FormControl(''),
+    age:         new FormControl('18'),
+    description: new FormControl(''),
+    category:    new FormControl('null'),
+    tag:         new FormControl('null'),
+    agree:       new FormControl(false),
+    gender:      new FormControl(''),
+    zone:        new FormControl('')
+  });
 
+  /* Ejemplo de uso de FormControl */
+  /*
   nameField  = new FormControl('', [ Validators.required, Validators.maxLength(10) ]);
   emailField = new FormControl('', [ Validators.required, Validators.email ]);
   phoneField = new FormControl('');
@@ -23,6 +40,7 @@ export class BasicFormComponent implements OnInit {
   agreeField  = new FormControl(false);
   genderField = new FormControl('');
   zoneField = new FormControl('');
+  */
 
   constructor() { }
 
@@ -38,6 +56,57 @@ export class BasicFormComponent implements OnInit {
       console.log(this.nameField.value);
   }
 
+  get nameField(){
+    return this.form.get('name');
+  }
+
+  //el nombre de la función get no importa, pero para que sea más facil le agregamos el nombre del elemrnto
+  get emailField(){
+    return this.form.get('email');
+  }
+
+  get phoneField(){
+    return this.form.get('phone');
+  }
+
+  get colorField(){
+    return this.form.get('color');
+  }
+
+  get dateField(){
+    return this.form.get('date');
+  }
+
+  get ageField(){
+    return this.form.get('age');
+  }
+
+  get descriptionField(){
+    return this.form.get('description');
+  }
+
+  get categoryField(){
+    return this.form.get('category');
+  }
+
+  get tagField(){
+    return this.form.get('tag');
+  }
+
+  get agreeField(){
+    return this.form.get('agree');
+  }
+
+  get genderField(){
+    return this.form.get('gender');
+  }
+
+  get zoneField(){
+    return this.form.get('zone');
+    zone:        new FormControl('')
+  }
+
+
   get isNameFieldValid(){
     return this.nameField.touched && this.nameField.valid;
   }
@@ -52,6 +121,10 @@ export class BasicFormComponent implements OnInit {
 
   get isEmailFieldInvalid(){
     return this.emailField.touched && this.emailField.invalid;
+  }
+
+  save(){
+    console.log(this.form.value);
   }
 
 }
