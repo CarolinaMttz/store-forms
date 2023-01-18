bb02cec9ccc3138dda13a51ef0e65d26ceb9381b
Dulce Carolina Martínez Carrera <dulce.martinez@ulsaoaxaca.edu.mx>
2023-01-17 22:18:03 -0600

    Clase Validaciones condicionadas y reactividad a variaciones en la UI

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -38,12 +38,26 @@ export class RegisterComponent implements OnInit {
 
   private buildForm() {
     this.form = this.formBuilder.group({
-      email: ['', [Validators.required]],
-      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(12), MyValidators.validatePassword]],
-      confirmPassword: ['', [Validators.required]]
+      email:           ['', [Validators.required]],
+      password:        ['', [Validators.required, Validators.minLength(6), Validators.maxLength(12), MyValidators.validatePassword]],
+      confirmPassword: ['', [Validators.required]],
+      type:            ['company', [Validators.required]],
+      companyName:     ['', [Validators.required]]
     },{
       validators: MyValidators.matchPassword
     });
+
+    this.typeField.valueChanges
+    .subscribe(value => {
+      console.log(value);
+      if( value === 'company'){
+          this.companyNameField.setValidators([Validators.required]);
+      }else{
+        this.companyNameField.setValidators(null);
+      }
+      this.companyNameField.updateValueAndValidity();
+    });
+
   }
 
   get emailField(){
@@ -57,4 +71,13 @@ export class RegisterComponent implements OnInit {
   get confirmPasswordField(){
     return this.form.get('confirmPassword');
   }
+
+  get typeField(){
+    return this.form.get('type');
+  }
+
+  get companyNameField(){
+    return this.form.get('companyName');
+  }
+
 }
